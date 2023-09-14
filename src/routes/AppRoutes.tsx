85277e79416ad7403e6a781e1dcbd032be148274
Jefferson Lima <Jefferson.fl123@gmail.com>
2023-09-14 12:38:11 -0300

    Configura react router e cria rotas da área não autenticada

---
+++ src/routes/AppRoutes.tsx
@@ -0,0 +1,36 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { routes } from '.';
+
+const isAutenticated = () => true; //TODO: Implementar função no authContext
+
+export const AppRoutes = () => {
+  return (
+    <Routes>
+      {isAutenticated() ? (
+        <>
+          <Route path={routes.home} element={<h1>Home placeholder</h1>} />
+          <Route path="*" element={<Navigate to={routes.home} />} />
+        </>
+      ) : (
+        <>
+          <Route path={routes.login} element={<h1>Login placeholder</h1>} />
+          <Route
+            path={routes.signUp.profile}
+            element={<h1>SignUp placeholder</h1>}
+          />
+          <Route
+            path={routes.signUp.address}
+            element={<h1>Add Address placeholder</h1>}
+          />
+          <Route
+            path={routes.recoverPassword}
+            element={<h1>Recover Password placeholder</h1>}
+          />
+          <Route path="*" element={<Navigate to={routes.login} />} />
+        </>
+      )}
+    </Routes>
+  );
+};
+
+//TODO: Adicionar pages de acordo com a criação das mesmas
