326d03117c61125a56be1d19cd4f626aa2c5a583
karine <karinedeveloper@gmail.com>
2023-09-15 11:19:51 -0300

    feat: adiciona GlobalStyle no app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { BrowserRouter } from 'react-router-dom';
 import { AppRoutes } from './routes/AppRoutes';
 import { ThemeProviderContext } from './contexts/themeContext';
+import { GlobalStyle } from './styles/global';
+
 function App() {
   return (
     <ThemeProviderContext>
       <BrowserRouter>
+        <GlobalStyle />
         <AppRoutes />
       </BrowserRouter>
     </ThemeProviderContext>
