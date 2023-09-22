5efd7b58a6c3ea5dc551c54a199f17cc1c8296e5
Jefferson Lima <Jefferson.fl123@gmail.com>
2023-09-22 17:17:14 -0300

    Adiciona função de configuração dos headers do axios

---
+++ src/utils/configureAxiosAuth.ts
@@ -0,0 +1,29 @@
+import secureLocalStorage from 'react-secure-storage';
+import { api } from '../service/api';
+import { refreshToken } from '../service/auth';
+
+export function configureAxiosToken(accessToken: string, refresh: string) {
+  api.interceptors.request.use(async function (config) {
+    let validToken = accessToken;
+    const tokenExpDate = secureLocalStorage.getItem('tokenExpDate');
+    if (tokenExpDate) {
+      const now = new Date();
+      const expDate = new Date(JSON.parse(tokenExpDate as string));
+      console.log(tokenExpDate);
+      console.log(now);
+      console.log(expDate);
+      if (expDate < now) {
+        secureLocalStorage.removeItem('tokenExpDate');
+        const data = await refreshToken(refresh);
+        console.log(data);
+        now.setHours(now.getHours() + 1);
+        validToken = data.access_token;
+        secureLocalStorage.setItem('token', validToken);
+        secureLocalStorage.setItem('refreshToken', data.refresh_token);
+        secureLocalStorage.setItem('tokenExpDate', JSON.stringify(now));
+      }
+    }
+    config.headers.Authorization = `Bearer ${validToken}`;
+    return config;
+  });
+}
