1054c99f12134d25d7944bb6c92574f47cb36b28
Jefferson Lima <Jefferson.fl123@gmail.com>
2023-09-24 19:03:51 -0300

    Corrige refresh_token flow

--- src/utils/configureAxiosAuth.ts
+++ src/utils/configureAxiosAuth.ts
@@ -6,36 +6,30 @@ export function configureAxiosToken(accessToken: string, refresh: string) {
   api.interceptors.request.use(async function (config) {
     let validToken = accessToken;
     const tokenExpDate = secureLocalStorage.getItem('tokenExpDate');
-    console.log(tokenExpDate);
     if (tokenExpDate) {
       const now = new Date();
       const expDate = new Date(JSON.parse(tokenExpDate as string));
-      console.log({ expDate, now });
       if (expDate < now) {
-        console.log('Sending refresh token request');
         secureLocalStorage.removeItem('tokenExpDate');
-        const data = await refreshToken(refresh);
-        console.log(data);
-        if (data.response.status === 401) {
-          secureLocalStorage.removeItem('refreshToken');
-          secureLocalStorage.removeItem('token');
-          secureLocalStorage.removeItem('user');
-        }
-        if (data.response.status === 200) {
+        try {
+          console.log('Sending refresh token request');
+          const data = await refreshToken(refresh);
           now.setHours(now.getHours() + 1);
           validToken = data.access_token;
           secureLocalStorage.setItem('token', validToken);
           secureLocalStorage.setItem('refreshToken', data.refresh_token);
           secureLocalStorage.setItem('tokenExpDate', JSON.stringify(now));
+          config.headers.Authorization = `Bearer ${validToken}`;
+        } catch (error) {
+          secureLocalStorage.removeItem('refreshToken');
+          secureLocalStorage.removeItem('token');
+          secureLocalStorage.removeItem('user');
+          return Promise.reject(error);
         }
       }
     }
-    // else {
-    //   secureLocalStorage.removeItem('refreshToken');
-    //   secureLocalStorage.removeItem('token');
-    //   secureLocalStorage.removeItem('user');
-    // }
+
     config.headers.Authorization = `Bearer ${validToken}`;
-    return config;
+    return Promise.resolve(config);
   });
 }
