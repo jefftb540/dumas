e96993a0fc62d7b38a0c9188935006de4766a2ee
carolina <carolinaarib10@gmail.com>
2023-09-28 19:29:30 -0300

    Tela de detalhes de produtos

--- src/pages/ProductDetails/index.tsx
+++ src/pages/ProductDetails/index.tsx
@@ -1,10 +1,13 @@
 import { Container, Title } from './styled';
 import { Button } from '../../components/Button';
+import { CardContainer } from '../../components/Card/styled';
 
 export const ProductDetails = () => {
   return (
     <Container>
+      <Title color="accent">Pratos próximos</Title>
       <Title> Pão com ovo</Title>
+      <CardContainer></CardContainer>
 
       <Button variant="primary" size="medium">
         Comprar
