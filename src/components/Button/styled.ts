9c79857c30b7333bfe402bbc2b080e9ff290ecfc
carolina <carolinaarib10@gmail.com>
2023-09-28 20:07:50 -0300

    🐛 - Corrige cor do botão

--- src/components/Button/styled.ts
+++ src/components/Button/styled.ts
@@ -10,7 +10,7 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
   border-radius: 30px;
   font-weight: ${({ theme }) => theme.fonts.weight.bold};
   font-size: ${({ theme }) => theme.fonts.text.small};
-  color: ${({ theme }) => theme.colors.text.light};
+  color: ${({ theme }) => theme.colors.white};
   padding: ${({ size }) => (size === 'medium' ? '12px 26px' : '18px 26px')};
   width: ${({ size }) => (size === 'medium' ? '150px' : '380px')};
   transition: 0.3s ease-in-out;
