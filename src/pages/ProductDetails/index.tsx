b114aabe8b754a4bc010ac3ed600af1680cea62f
carolina <carolinaarib10@gmail.com>
2023-09-28 23:53:26 -0300

    ✨ - Carrega detalhes do produto com base no id da rota

--- src/pages/ProductDetails/index.tsx
+++ src/pages/ProductDetails/index.tsx
@@ -1,13 +1,35 @@
-import { Container, Title } from './styled';
+import { useEffect, useState } from 'react';
+import { Container } from './styled';
+import { Title } from '../../components/Title';
 import { Button } from '../../components/Button';
-import { CardContainer } from '../../components/Card/styled';
+import { Dish } from '../../types/Dish';
+import { getDishId } from '../../service/api/dishes';
+import { useParams } from 'react-router-dom';
 
-export const ProductDetails = () => {
+export const ProductDetails = ({ dishes, type }: ProductDetailsProps) => {
+  const [dishDetail, setDishDetail] = useState<Dish>();
+  let { id } = useParams();
+
+  useEffect(() => {
+    const getData = async () => {
+      const dishDetailData = await getDishId(id);
+      setDishDetail(dishDetailData);
+    };
+    getData();
+  }, []);
+
+
   return (
     <Container>
-      <Title color="accent">Pratos próximos</Title>
-      <Title> Pão com ovo</Title>
-      <CardContainer></CardContainer>
+      <Title color="accent">{dishDetail?.name}</Title>
+
+      <Title color="accent">{dishDetail?.chef.name}</Title>
+      <Title color="accent">{dishDetail?.available}</Title>
+      <Title color="accent">{dishDetail?.description}</Title>
+      <Title color="accent">{dishDetail?.unit_price}</Title>
+      <Title color="accent">{dishDetail?.distance}</Title>
+      <img src={dishDetail.images[0]} />
+
 
       <Button variant="primary" size="medium">
         Comprar
--- src/pages/ProductDetails/styled.ts
+++ src/pages/ProductDetails/styled.ts
@@ -1,17 +1,25 @@
-import { styled } from 'styled-components';
+import styled from 'styled-components';
 
 export const Container = styled.div`
-  display: flex;
+  /* display: flex;
   padding-top: 20px;
   flex-direction: column;
   justify-content: center;
-  align-items: center;
+  align-items: center; */
+
+  /* display: flex;
+  justify-content: space-between; */
+
+  padding: 12px 32px;
+  max-width: 100%;
+  display: flex;
+  flex-direction: column;
 `;
 
-export const Title = styled.h1`
-  width: 350px;
-  font-size: ${({ theme }) => theme.fonts.text.medium};
-  font-style: italic;
-  font-weight: ${({ theme }) => theme.fonts.weight.bold};
-  color: ${({ theme }) => theme.colors.text.main};
-`;
+// export const Title = styled.h1`
+//   width: 350px;
+//   font-size: ${({ theme }) => theme.fonts.text.medium};
+//   font-style: italic;
+//   font-weight: ${({ theme }) => theme.fonts.weight.bold};
+//   color: ${({ theme }) => theme.colors.text.main};
+// `;
