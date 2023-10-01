aabe22590fcc2e7382cb68c6cb1208e7bb1d1b94
carolina <carolinaarib10@gmail.com>
2023-10-01 15:47:33 -0300

    ✨ - Estiliza scroll

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,22 +1,22 @@
 import { createGlobalStyle } from 'styled-components';
-import 'react-toastify/dist/ReactToastify.css';
 
 export const GlobalStyle = createGlobalStyle`
-* {
-  margin: 0;
-  padding: 0;
-  outline: 0;
-  box-sizing: border-box;
-  line-height: 1.5;
-}
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+    line-height: 1.5;
+  }
 
-html, body {
-  width: 100%;
-  min-height: 100vh;
-  font-size: 100%;
-  font-family: ${({ theme }) => theme.fonts.family.primary};
-  -webkit-font-smoothing: antialiased;
-  background-color: ${({ theme }) => theme.colors.background.light}; ;
+  html, body {
+    width: 100%;
+    min-height: 100vh;
+    font-size: 100%;
+    font-family: ${({ theme }) => theme.fonts.family.primary};
+    -webkit-font-smoothing: antialiased;
+    background-color: ${({ theme }) => theme.colors.background.light};
+  }
 
   button {
     cursor: pointer;
@@ -30,6 +30,15 @@ html, body {
   ul {
     list-style-type: none;
   }
-}
 
+  ::-webkit-scrollbar {
+width: 10px ;
+
+  }
+
+::-webkit-scrollbar-thumb {
+  width: 10px ;
+  background-color: ${({ theme }) => theme.colors.primary.main}; 
+  border-radius: 6px;
+} 
 `;
